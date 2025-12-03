3913ef1e9941ef8b06632ed97eed38c742e6c77f
rkukuczka <rafal.qqczka@gmail.com>
2025-12-03 14:41:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+namespace FolderSync
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: FolderSync <source> <destination>");
+                return;
+            }
+
+            string sourcePath = args[0];
+            string targetPath = args[1];
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+
+            Console.WriteLine($"Syncing {sourcePath} to {targetPath}");
+
+            Stopwatch watch = Stopwatch.StartNew();
+
+            try
+            {
+                Synchronizator synchronizator = new Synchronizator();
+                synchronizator.Synchronize(sourcePath, targetPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally { 
+                watch.Stop();
+                Console.WriteLine($"Synchronization complete in {watch.Elapsed} sec.");
+            }
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine("Exception:");
+
+            if (e.ExceptionObject is Exception)
+                Console.WriteLine(((Exception)e.ExceptionObject).Message);
+            else
+                Console.WriteLine(e.ExceptionObject.ToString());
+        }
+
+        private static void TaskScheduler_UnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Console.WriteLine("Exception:");
+            Console.WriteLine(e.Exception.Message);
+            e.SetObserved();
+        }
+    }
+}
---
+++ Synchronizator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSync
+{
+    internal class Synchronizator
+    {
+        public static (HashSet<string> dirs, HashSet<(string file, string tick)> files) GetSnapshot(string dir)
+        {
+            
+            string[] directories = GetDirectories(dir);
+
+            var dirHash = directories.Select(GetRelativePath).ToHashSet();
+
+            var fileHash = directories.AsParallel().SelectMany(Directory.EnumerateFiles)
+               .Select(file => (name: GetRelativePath(file), ticks: GetHash(file)))
+               .ToHashSet();
+
+            return (dirHash, fileHash);
+
+            static string[] GetDirectories(string dir)
+            {
+                var directories = Directory.GetDirectories(dir, "*", SearchOption.AllDirectories);
+                directories = directories.Append(dir).ToArray();
+                return directories;
+            }
+
+            string GetHash(string path) => GetMd5(path);
+
+            string GetMd5(string path)
+            {
+                using var md5 = System.Security.Cryptography.MD5.Create();
+                using var stream = File.OpenRead(path);
+                var hash = md5.ComputeHash(stream);
+                return Convert.ToHexString(hash);
+            }
+
+            string GetRelativePath(string path) => path.Substring(dir.Length).TrimStart('/', '\\');
+        }
+
+        public void Synchronize(string sourcePath, string dstPath)
+        {
+            var snapshotSrc = GetSnapshot(sourcePath);
+            var snapshotDst = GetSnapshot(dstPath);
+
+            var filesToRemove = snapshotDst.files.Where(x => !snapshotSrc.files.Contains(x));
+            foreach (var fileToRemove in filesToRemove)
+            {
+                Console.WriteLine("Deleting file: " + Path.Combine(dstPath, fileToRemove.file));
+                File.Delete(Path.Combine(dstPath, fileToRemove.file));
+            }
+
+            var dirsToRemove = snapshotDst.dirs.Where(x => !snapshotSrc.dirs.Contains(x));
+            foreach (var dirToRemove in dirsToRemove)
+            {
+                Console.WriteLine("Deleting dir: " + Path.Combine(dstPath, dirToRemove));
+                Directory.Delete(Path.Combine(dstPath, dirToRemove), true);
+            }
+
+            var dirsToAdd = snapshotSrc.dirs.Where(x => !snapshotDst.dirs.Contains(x));
+            foreach (var dirToAdd in dirsToAdd)
+            {
+                Console.WriteLine("Adding dir: " + Path.Combine(dstPath, dirToAdd));
+                Directory.CreateDirectory(Path.Combine(dstPath, dirToAdd));
+            }
+
+            var filesToAdd = snapshotSrc.files.Where(x => !snapshotDst.files.Contains(x));
+            foreach (var fileToAdd in filesToAdd)
+            {
+                Console.WriteLine("Adding file: " + Path.Combine(dstPath, fileToAdd.file));
+                File.Copy(Path.Combine(sourcePath, fileToAdd.file), Path.Combine(dstPath, fileToAdd.file), true);
+            }
+
+        }
+    }
+}
